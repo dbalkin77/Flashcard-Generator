0b13f38f011a583949d22b5874e4bd81280fe77a
Douglas Balkin <dbalkin77@gmail.com>
2017-07-26 13:33:40 -0500

    constructors created

---
+++ BasicCard.js
@@ -0,0 +1,8 @@
+function Basic (front, back) {
+    this.front = front;
+    this.back = back
+}
+
+
+
+module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,7 @@
+function Close (text, close) {
+    this.text = text;
+    this.close = close
+}
+
+
+module.exports = ClozeCard;
